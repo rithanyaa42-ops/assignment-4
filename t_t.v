0e0fb1ba12ad336584193a7ba33e8d83361781d6
rithanyaa42-ops <rithanyaa42@gmail.com>
2025-12-30 11:55:01 +0530

    Update t_t.v

--- t_t.v
+++ t_t.v
@@ -1,1 +1,29 @@
+`timescale 1ns/1ps
+
+module t_ff(input t, clk, rst, output reg q);
+always @(posedge clk or posedge rst) begin
+    if (rst)      q <= 1'b0;
+    else if (t)  q <= ~q;
+end
+endmodule
+
+module tb_t_ff;
+reg t, clk, rst;
+wire q;
+
+t_ff dut(t, clk, rst, q);
+
+always #5 clk = ~clk;
+
+initial begin
+    clk=0; rst=1; t=0;
+    #10 rst=0;
+    #10 t=1;
+    #20 t=0;
+    #10 t=1;
+    #20 $finish;
+end
+endmodule
+
+
 
